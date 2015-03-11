ab1193be4041d2a2a48f99809940c049d4e44a11
TheStevious <stepkenny@gmail.com>
2015-03-11 11:18:11 +0000

    Update worker.js

--- worker.js
+++ worker.js
@@ -24,47 +24,29 @@ pg.connect(process.env.DATABASE_URL+'?ssl=true', function(err, client, done) {
         contacts[row.twitter_handle__c] = row;
       });
       console.log('contacts :', contacts);
+	  tw.stream(function(stream) {
+		stream.on('data', function(tweet) {
+		  console.log('Tweet: ',tweet);
+		  if (contacts[tweet.user.screen_name]) {
+			console.log('Inserting: ', tweet.id_str, contacts[tweet.user.screen_name].sfid, tweet.text);
+			var insert = 'INSERT INTO salesforce.tweet__c(name, contact__c, text__c) '+
+						 'VALUES($1, $2, $3)';
+			client.query(insert, [tweet.id_str, contacts[tweet.user.screen_name].sfid, tweet.text], function(err, result) {
+			  if(err) {
+				console.error(err);
+			  } else {
+				console.log('Inserted: ', tweet.id_str);
+			  }
+			});
+		  }
+		});
+	   
+		stream.on('error', function(error) {
+		  console.error(error);
+		  throw error;
+		});
+	  }); 
 
-      //client.query('SELECT hashtag__c, sfid FROM salesforce.campaign WHERE campaign.hashtag__c IS NOT NULL', function(err, result) {
-        //if (err) { 
-        //  console.error(err);
-        //} else {
-          var campaigns = result.rows;
-          var query = '';
-          result.rows.forEach(function(row){
-            query += ((query === '') ? '' : ',')+row.hashtag__c;
-          });
-          console.log('query: ', query);
-          console.log('campaigns: ', campaigns);
-
-          tw.stream('statuses/filter', {track: query}, function(stream) {
-            stream.on('data', function(tweet) {
-              console.log('Tweet: ',tweet);
-              if (contacts[tweet.user.screen_name]) {
-                //campaigns.forEach(function(campaign){
-                  //if (tweet.text.toLowerCase().indexOf(campaign.hashtag__c.toLowerCase()) !== -1) {
-                    console.log('Inserting: ', tweet.id_str, contacts[tweet.user.screen_name].sfid, tweet.text);
-                    var insert = 'INSERT INTO salesforce.tweet__c(name, contact__c, text__c) '+
-                                 'VALUES($1, $2, $3)';
-                    client.query(insert, [tweet.id_str, contacts[tweet.user.screen_name].sfid,tweet.text], function(err, result) {
-                      if(err) {
-                        console.error(err);
-                      } else {
-                        console.log('Inserted: ', tweet.id_str);
-                      }
-                    });
-                  //}
-                //});
-              }
-            });
-           
-            stream.on('error', function(error) {
-              console.error(error);
-              throw error;
-            });
-          }); 
-        //}
-      //});
     }
   });
 });
